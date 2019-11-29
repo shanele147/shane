8e2279e7d80357d7b7f4347f89c848f3fc431f91
shane <shane.le147@gmail.com>
2019-11-30 00:20:14 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      */}
-      <div class="img-container"> Hello
-      {/* <img src='img/chair.jpg'></img> */}
+      <p>Hello ReactJS !</p>
+      <p>Shane</p>
+      {/* <div class="img-container"> 
+      <img src='img/chair.jpg'></img>
       </div>
 
       <div class="sale-bar">SALE</div>
@@ -15,7 +16,7 @@ function App() {
         <h3>FURNITURE</h3>
         <p class="promotion-text">Minimal Decor Furniture</p>
         <p><span class="sale-price">$119.00 USD</span><span class="normal-price">$230.00 USD</span></p>
-      </div>
+      </div> */}
     </div>
 
   );
