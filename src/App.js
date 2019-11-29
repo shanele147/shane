38f6c43290791c63b1c3c261325f20cbe4d2043c
shane <shane.le147@gmail.com>
2019-11-29 23:52:09 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          <p>Hello ReactJS !</p>
-          <p>Shane</p>
+      <p>Hello ReactJS !</p>
+      <p>Shane</p>
+      <div class="img-container"> <img src='img/chair.jpg'></img></div>
+
+      <div class="sale-bar">SALE</div>
+      <div class="promotion-content">
+        <h3>FURNITURE</h3>
+        <p class="promotion-text">Minimal Decor Furniture</p>
+        <p><span class="sale-price">$119.00 USD</span><span class="normal-price">$230.00 USD</span></p>
+      </div>
     </div>
+
   );
 }
 
